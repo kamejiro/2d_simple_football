08f014db1d3fa72660860d88dc56a40e37020e7d
kamejiro <kameoutput0529@gmail.com>
2024-01-01 19:30:11 +0900

    チーム情報追加

--- Assets/Scripts/ButtonTest.cs
+++ Assets/Scripts/ButtonTest.cs
@@ -15,8 +15,10 @@ public class ButtonTest : MonoBehaviour
     private List<List<string>> csvData = new List<List<string>>();
 
     public GameObject player;
+    public GameObject team;
 
     [SerializeField] Text viewText;
+    [SerializeField] Text viewText2;
 
     public void OnClick()
     {
@@ -53,16 +55,33 @@ public class ButtonTest : MonoBehaviour
 
     void AttachCSVData()
     {
+        //チームデータの入力
+        GameObject obj2 = Instantiate(team) as GameObject;
+        TeamData teamData = obj2.GetComponent<TeamData>();
+        teamData.team_name = "Liverpool";
+        teamData.score = 0;
+        teamData.Total_ATK = 0;
+        teamData.Total_DEF = 0;
+
         foreach (var row in csvData)
         {
+            //プレイヤーデータの入力
             GameObject obj=Instantiate(player) as GameObject;
             MonsterData monsterData = obj.GetComponent<MonsterData>();
             monsterData.player_name = row[0];
             monsterData.position = row[1];
             monsterData.ATK = int.Parse(row[2]);
             monsterData.DEF = int.Parse(row[3]);
-            Debug.Log(monsterData.player_name + "のhpは" + monsterData.position + "のatkは" + monsterData.ATK + "のdefは" + monsterData.DEF);
-            viewText.text = monsterData.player_name + "のhpは" + monsterData.position + "のatkは" + monsterData.ATK + "のdefは" + monsterData.DEF;
+
+            teamData.Total_ATK += int.Parse(row[2]);
+            teamData.Total_DEF += int.Parse(row[3]);
+
+            //プレイヤー出力
+            Debug.Log(monsterData.player_name + "のposは" + monsterData.position + "のatkは" + monsterData.ATK + "のdefは" + monsterData.DEF);
+            viewText.text += monsterData.position + " " + monsterData.player_name + "\n";
         }
+        //チーム出力
+        Debug.Log(teamData.team_name + "のscoreは" + teamData.score + "のatkは" + teamData.Total_ATK + "のdefは" + teamData.Total_DEF);
+        viewText2.text += teamData.team_name + " " + teamData.score;
     }
 }
---
+++ Assets/Scripts/TeamData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class TeamData : MonoBehaviour
+{
+    public string team_name;
+    public int score;
+    public int Total_ATK;
+    public int Total_DEF;
+}
